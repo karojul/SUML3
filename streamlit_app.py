3bc9ba484d6833e52501383067059ba43dc38fec
karolina <s26414@pjwstk.edu.pl>
2026-04-25 04:47:10 +0200

    xxx

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import os
 # zaczynamy od zaimportowania bibliotek
 
 # st.info('Gratulacje! Z powodzeniem uruchomiłeś aplikację')
-# streamlit jest wykorzystywany do tworzenia aplikacji
+# .streamlit jest wykorzystywany do tworzenia aplikacji
 # z tego powodu dobrą praktyką jest informowanie użytkownika o postępie, błędach, etc.
 
 # Inne przykłady do wypróbowania:
@@ -36,7 +36,7 @@ st.title('Analiza i tłumaczenie tekstów')
 st.header('Instrukcja')
 st.subheader('Jak działa tłumacz')
 st.text('Skorzystaj z naszego narzędzia do analizy emocji w tekście lub przetłumaczenie zdań z angielskiego na niemiecki! ')
-st.write('Streamlit jest biblioteką pozwalającą na uruchomienie modeli uczenia maszynowego.')
+# st.write('Streamlit jest biblioteką pozwalającą na uruchomienie modeli uczenia maszynowego.')
 
 # df = pd.read_csv("../../Downloads/DSP_4.csv", sep =';')
 # st.dataframe(df)
@@ -91,12 +91,12 @@ st.subheader('Zadanie do wykonania')
 st.write('Wykorzystaj Huggin Face do stworzenia swojej własnej aplikacji tłumaczącej tekst z języka angielskiego na język niemiecki. '
          'Zmodyfikuj powyższy kod dodając do niego kolejną opcję, tj. tłumaczenie tekstu. Informacje potrzebne do zmodyfikowania kodu '
          'znajdziesz na stronie Huggin Face - https://huggingface.co/docs/transformers/index')
-st.write('🐞 Dodaj właściwy tytuł do swojej aplikacji, może jakieś grafiki?')
-st.write('🐞 Dodaj krótką instrukcję i napisz do czego służy aplikacja')
-st.write('🐞 Wpłyń na user experience, dodaj informacje o ładowaniu, sukcesie, błędzie, itd.')
-st.write('🐞 Na końcu umieść swój numer indeksu')
-st.write('🐞 Stwórz nowe repozytorium na GitHub, dodaj do niego swoją aplikację, plik z wymaganiami (requirements.txt)')
-st.write('🐞 Udostępnij stworzoną przez siebie aplikację (https://share.streamlit.io) a link prześlij do prowadzącego')
+# st.write('🐞 Dodaj właściwy tytuł do swojej aplikacji, może jakieś grafiki?')
+# st.write('🐞 Dodaj krótką instrukcję i napisz do czego służy aplikacja')
+# st.write('🐞 Wpłyń na user experience, dodaj informacje o ładowaniu, sukcesie, błędzie, itd.')
+# st.write('🐞 Na końcu umieść swój numer indeksu')
+# st.write('🐞 Stwórz nowe repozytorium na GitHub, dodaj do niego swoją aplikację, plik z wymaganiami (requirements.txt)')
+# st.write('🐞 Udostępnij stworzoną przez siebie aplikację (https://share.streamlit.io) a link prześlij do prowadzącego')
 
 st.divider()
 st.caption("Numer indeksu: s26414")
